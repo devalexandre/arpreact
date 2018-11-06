aed31be8b2856032ed5e209bfab24992d309aba1
Alexandre Souza <alexandre@indev.net.br>
2018-11-06 01:11:57 -0200

    alter maker

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ class App extends Component {
 
 	render() {
 		return (
-			<a-scene embedded arjs='sourceType: webcam;' >
+			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;' >
 				<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-				<a-marker-camera preset='hiro'></a-marker-camera>
+				<a-marker-camera  type="patter" patterUrl="./assets/indev.patt"></a-marker-camera>
 				</a-scene>
 		);
 	}
 }
+
+
 
 render(<App />, document.body);
