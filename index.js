eb714da52d0e42bf493e4b83553a758cc00a3f7f
Alexandre Souza <alexandre@indev.net.br>
2018-11-06 00:07:33 -0200

    teste iniciado

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import './style';
-import { Component } from 'preact';
+import { h,Component,render } from 'preact';
 
  
-export default class App extends Component {
+class App extends Component {
 
 	render() {
 		return (
-		<a-scene embedded arjs='sourceType: webcam;'>
+			<a-scene embedded arjs='sourceType: webcam;' >
 				<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
 				<a-marker-camera preset='hiro'></a-marker-camera>
-			</a-scene>
+				</a-scene>
 		);
 	}
 }
+
+render(<App />, document.body);
