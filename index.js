3b4f95583d7254b3cfe92be6601adb66bc1cf0a1
Alexandre Souza <alexandre@indev.net.br>
2018-11-06 15:02:32 -0200

    teste

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ class App extends Component {
 	render() {
 		return (
 			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false; ' >
-					<a-marker preset='custom' type="pattern" url='https://raw.githubusercontent.com/devalexandre/arpreact/master/assets/indev.patt'>
-					<a-box position='0 0.5 0' material='color: purple;'></a-box>
+						<a-assets>
+					<img src="./assets/heart.jpeg" id="heart" />
+					</a-assets>
+					<a-marker preset='hiro'>
+				
+					<a-image src="#heart" width="2" height="2" position='0 0.5 0' rotation="-90 0 0 " text="value: Maby My Love; color:#000; align: center"></a-image>
+        
 					</a-marker>
 					<a-entity camera></a-entity>
 				</a-scene>
