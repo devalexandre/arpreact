747965b9ab5504b80502d063f0e749180f4646da
Alexandre Souza <alexandre@indev.net.br>
2018-11-06 19:48:26 -0200

    usando AR video

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 import './style';
 import { h,Component,render } from 'preact';
-
+import heart from './assets/heart.jpeg'
  
 class App extends Component {
 
+	 play() {
+		const vid = document.getElementById('video');
+
+		vid.play();
+	
+	}
 	render() {
 		return (
+			
+
 			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false; ' >
 						<a-assets>
-					<video src="./assets/Sobre_os_jovens_se_manifestarem.mp4" id="video" autoplay loop="true" webkit-playinline/>
+					<img src={heart} id="heart" />
+					<video src="./assets/Sobre_os_jovens_se_manifestarem.mp4" id="video"  loop="true" webkit-playinline/>
+					
 					</a-assets>
+		
+
 					<a-marker preset='hiro'>
-				
-					<a-video src="#video" width="16" height="9" position="0 0 -20"></a-video>
+				<a-video src="#video" width="2" height="2" position='0 0.5 0' onClick={() => this.play()}></a-video>
+
 					</a-marker>
-					<a-entity camera></a-entity>
+					<a-entity camera>
+						<a-cursor color="#ff0000"></a-cursor>
+					</a-entity>
 				</a-scene>
 		);
 	}
