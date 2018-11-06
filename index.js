7be3f0087b202a819e37c905603b632912cc61eb
Alexandre Souza <alexandre@indev.net.br>
2018-11-06 21:16:30 -0200

    start

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 import './style';
 import { h,Component,render } from 'preact';
-import heart from './assets/heart.jpeg'
+import bg from './assets/bg.jpg'
  
 class App extends Component {
 
-	 play() {
-		const vid = document.getElementById('video');
 
-		vid.play();
-	
-	}
 	render() {
 		return (
 			
-
-			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false; ' >
+			<a-scene >
 						<a-assets>
-					<img src={heart} id="heart" />
-					<video src="./assets/Sobre_os_jovens_se_manifestarem.mp4" id="video"  loop="true" webkit-playinline/>
-					
+					<img src={bg} id="sky" />
+
 					</a-assets>
-		
+	
+				<a-sky src="#sky"></a-sky>
 
-					<a-marker preset='hiro'>
-				<a-video src="#video" width="2" height="2" position='0 0.5 0' onClick={() => this.play()}></a-video>
-
-					</a-marker>
-					<a-entity camera>
-						<a-cursor color="#ff0000"></a-cursor>
-					</a-entity>
 				</a-scene>
 		);
 	}
