b878fce2646c0ca5866215342fa909df6076536f
Alexandre Souza <alexandre@indev.net.br>
2018-11-05 23:28:34 -0200

    esta com bug

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import { Component } from 'preact';
 
  
 export default class App extends Component {
+
 	render() {
 		return (
-
-			<a-scene embedded arjs='sourceType: webcam;'>
-			<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-			<a-marker-camera preset='hiro'></a-marker-camera>
-		</a-scene>
-	
+		<a-scene embedded arjs='sourceType: webcam;'>
+				<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+				<a-marker-camera preset='hiro'></a-marker-camera>
+			</a-scene>
 		);
 	}
 }
