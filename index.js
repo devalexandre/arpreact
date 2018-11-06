1c5b6e4eb85cb90122f6f160cfdda6496afba12f
Alexandre Souza <alexandre@indev.net.br>
2018-11-06 12:50:47 -0200

    custom patt

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ class App extends Component {
 
 	render() {
 		return (
-			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: true; patternRatio: 0.50' >
-					<a-marker type='pattern'  patternUrl="https://raw.githubusercontent.com/devalexandre/arpreact/master/assets/indev.patt">
-						<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-						</a-marker>
-						<a-entity camera></a-entity>
+			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false; ' >
+					<a-marker preset='custom' type="pattern" url='https://raw.githubusercontent.com/devalexandre/arpreact/master/assets/indev.patt'>
+					<a-box position='0 0.5 0' material='color: purple;'></a-box>
+					</a-marker>
+					<a-entity camera></a-entity>
 				</a-scene>
 		);
 	}
