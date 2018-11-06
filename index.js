6a2dbc7ddf8912200e27a5c0048564f81fee615c
Alexandre Souza <alexandre@indev.net.br>
2018-11-06 12:02:47 -0200

    alter marker

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ class App extends Component {
 
 	render() {
 		return (
-			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;' >
-				<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-				<a-marker-camera  type="patter" patterUrl="./assets/indev.patt"></a-marker-camera>
+			<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: true; patternRatio: 0.50' >
+					<a-marker type='pattern'  patternUrl="https://raw.githubusercontent.com/devalexandre/arpreact/master/assets/indev.patt">
+						<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+						</a-marker>
+						<a-entity camera></a-entity>
 				</a-scene>
 		);
 	}
