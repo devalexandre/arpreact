3110513eb73cca76a9cfaa4e63dc90bd11a42d56
Alexandre Souza <alexandre@indev.net.br>
2018-11-07 10:55:30 -0200

    teste marker

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import './style';
 import { h,Component,render } from 'preact';
-import bg from './assets/bg.jpg'
+
  
 class App extends Component {
 
@@ -8,18 +8,16 @@ class App extends Component {
 	render() {
 		return (
 			
-			<a-scene >
-						<a-assets>
-					<img src={bg} id="sky" />
-
-					</a-assets>
-	
-				<a-sky src="#sky"></a-sky>
-
-				</a-scene>
+			<a-scene embedded arjs='sourceType: webcam;'>
+  	<a-marker type="pattern" patternUrl='./assets/marker.patt'>
+            <a-box position='0 0.5 0' material='color: black;'></a-box>
+  	</a-marker>
+  	<a-entity camera></a-entity>
+    </a-scene>
 		);
 	}
 }
+
 
 
 
