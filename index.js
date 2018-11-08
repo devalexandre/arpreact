97218676017d5bfd5023467aea367a5da9d35d1a
Alexandre Souza <alexandre@indev.net.br>
2018-11-08 07:39:34 -0200

    maker custom

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class App extends Component {
 		return (
 			
 			<a-scene embedded arjs='sourceType: webcam;'>
-  	<a-marker type="pattern" patternUrl='https://raw.githubusercontent.com/devalexandre/arpreact/master/assets/marker.patt'>
+  	<a-marker type="pattern" preset='custom' url='https://raw.githubusercontent.com/devalexandre/arpreact/master/assets/marker.patt'>
             <a-box position='0 0.5 0' material='color: black;'></a-box>
   	</a-marker>
   	<a-entity camera></a-entity>
