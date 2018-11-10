1f965f0e65fee85d4c0add5952987e40c9ad59f5
Alexandre Souza <alexandre@indev.net.br>
2018-11-10 14:43:11 -0200

    teste 1 marker

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ class App extends Component {
 	render() {
 		return (
 			
-			<a-scene embedded arjs='sourceType: webcam;'>
-  	<a-marker type="pattern" preset='custom' url='https://raw.githubusercontent.com/devalexandre/arpreact/master/assets/marker.patt'>
-            <a-box position='0 0.5 0' material='color: black;'></a-box>
-  	</a-marker>
-  	<a-entity camera></a-entity>
-    </a-scene>
+<a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;'>
+    <a-marker type="pattern" url="./assets/marker.patt">
+      <a-box position='0 0 0' material='opacity: 0.5; side:double; color:green;'></a-box>
+    </a-marker>
+    <a-camera-static/>
+</a-scene>
 		);
 	}
 }
