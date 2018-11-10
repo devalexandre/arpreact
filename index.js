eb9d5b312a2565f7aaa4374158773df8c42c8d4d
Alexandre Souza <alexandre@indev.net.br>
2018-11-10 15:07:28 -0200

    teste 3 cake with scale

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ class App extends Component {
 			
 <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;'>
 <a-assets>
-    <a-asset-item id="cake" src="./assets/produtos/cake.glb"></a-asset-item>
+    <a-asset-item id="cake" src='./assets/produtos/cake.glb'></a-asset-item>
   </a-assets>
 
 
     <a-marker type="pattern" url="./assets/marker.patt">
-	<a-entity gltf-model="#cake" position='0 0 0'></a-entity>
+	<a-entity gltf-model="url(./assets/produtos/cake.glb)" scale="4 4 4" position='0 0 0'></a-entity>
     
     </a-marker>
     <a-camera-static/>
