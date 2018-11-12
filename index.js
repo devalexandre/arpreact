0e3b62a0db53bd2f26ae007e1ed1b2b19f557054
Alexandre Souza <alexandre@indev.net.br>
2018-11-12 10:54:17 -0200

    teste android

--- index.js
+++ index.js
@@ -4,26 +4,6 @@ import { h,Component,render } from 'preact';
  
 class App extends Component {
 
-    constructor(){
-        super();
-        this.state = {
-            "products":[
-                {"name":"cake","position":1},
-                {"name":"pizza","position":0},
-            ],
-            "ativado":"cake"
-        }
-
-
-    }
-
-
-    mudar(produto){
-    
-            this.setState({"ativado":produto.name})
-        }
-    
-
 
 
 	render() {
@@ -31,29 +11,15 @@ class App extends Component {
 			
 <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;'>
 
-
-
-  <a-assets>
-    <img id="next" src="./assets/next.png"/>
-    <img id="prev" src="./assets/next.png"/>
-  </a-assets>
-
-{
-    this.state.products.map(produto =>(
-        <a-image onClick={ ()=> this.mudar(produto)}  src={`./assets/produtos/${produto.name}.png`} width="1" height="1" position={`-4.5 ${produto.position} -10`} ></a-image>
-
-    ))
-}
-
     <a-marker type="pattern" url="./assets/marker.patt">
    
     <a-entity 
-    gltf-model={`url(./assets/produtos/${this.state.ativado}.glb)`} 
+    gltf-model={`url(./assets/produtos/pizza.glb)`} 
     
     scale="4 4 4" position='0 0 0'></a-entity>
   
     </a-marker>
-    <a-camera-static look-controls cursor="rayOrigin: mouse"/>
+    <a-camera-static />
 </a-scene>
 		);
 	}
