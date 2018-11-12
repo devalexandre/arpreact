a13236daa537e7031eca741c7fc6581d3a27c5ab
Alexandre Souza <alexandre@indev.net.br>
2018-11-12 09:23:43 -0200

    teste sem controllers

--- index.js
+++ index.js
@@ -31,20 +31,6 @@ class App extends Component {
 			
 <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;'>
 
-<a-entity id="camera" look-controls cursor="rayOrigin: mouse"></a-entity>
-
-  <a-assets>
-    <img id="next" src="./assets/next.png"/>
-    <img id="prev" src="./assets/next.png"/>
-  </a-assets>
-
-{
-    this.state.products.map(produto =>(
-        <a-image onClick={ ()=> this.mudar(produto)}  src={`./assets/produtos/${produto.name}.png`} width="1" height="1" position={`-4.5 ${produto.position} -10`} ></a-image>
-
-    ))
-}
-
     <a-marker type="pattern" url="./assets/marker.patt">
    
     <a-entity 
@@ -53,7 +39,7 @@ class App extends Component {
     scale="4 4 4" position='0 0 0'></a-entity>
   
     </a-marker>
-    
+  
 </a-scene>
 		);
 	}
